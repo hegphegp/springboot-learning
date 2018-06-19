a1755fee5f21e6a9a69a14326685c80f3082aa9f
heguopei <heguopei@ascs.tech>
2018-06-19 20:31:09 +0800

    添加swagger分组配置的方法

---
+++ springboot-swagger/src/main/java/tech/ascs/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package tech.ascs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration  //让Spring来加载该类配置
+@EnableSwagger2 //启用Swagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket alipayApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("支付宝API接口文档")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.itstyle.modules.alipay"))
+            .paths(PathSelectors.any()).build();
+    }
+
+    @Bean
+    public Docket weixinpayApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("微信API接口文档")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.itstyle.modules.weixinpay"))
+            .paths(PathSelectors.any()).build();
+    }
+
+    @Bean
+    public Docket unionpayApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("银联API接口文档")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.itstyle.modules.unionpay"))
+            .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("支付系统")
+            .description("微信、支付宝、银联支付服务")
+            .termsOfServiceUrl("http://blog.52itstyle.com")
+            .contact(new Contact("科帮网 ", "http://blog.52itstyle.com", "345849402@qq.com"))
+            .version("1.0").build();
+    }
+
+}
