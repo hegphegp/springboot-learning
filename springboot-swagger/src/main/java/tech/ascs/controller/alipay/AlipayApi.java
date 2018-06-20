e04e0b687c5cb7ac98ae559371a9babc8bcfa39c
heguopei <heguopei@ascs.tech>
2018-06-20 13:49:47 +0800

    springfox-swagger-ui的界面越做越烂了，官方开始追求华而不实的功能了，原来一个页面可以显示的内容，高版本要两三个页面来显示

--- springboot-swagger/src/main/java/tech/ascs/controller/alipay/AlipayApi.java
+++ springboot-swagger/src/main/java/tech/ascs/controller/alipay/AlipayApi.java
@@ -6,12 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.constraints.NotNull;
-
 @Api(value = "alipay", description = "阿里支付类的接口")
 public interface AlipayApi {
     @ApiOperation(value = "阿里支付类的查询接口", nickname = "阿里支付类的查询接口", response = Domain.class, tags={ "alipay" })
 //    @ApiResponse(code = 200, message = "successful operation", response = Domain.class)
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "book's name", required = true, dataType = "string", paramType = "query"),
+        @ApiImplicitParam(name = "typeId", value = "book's name", required = true, dataType = "string", paramType = "query"),
+        @ApiImplicitParam(name = "date", value = "book's date", required = false, dataType = "string", paramType = "query")})
     @PostMapping("/alipay/v1/{typeId}/query")
-    ResponseEntity<Void> query(@NotNull @ApiParam(value = "产品类型ID", required=true) @PathVariable("typeId") String typeId);
+    ResponseEntity<Void> query(@PathVariable("typeId") String typeId);
 }
